7538472d15607b56b7582cf3b3ab2fc8d90b6562
Miron Veryanskiy <miron@iheartengineering.com>
2012-08-31 20:02:31 -0400

    Created Arduino Scripts.  Updated BOM to include 12Ohm resistor

---
+++ PCB_RGB_HEXAlight/ArduinoCodes/ConstantWhite/ConstantWhite.ino
@@ -0,0 +1,16 @@
+//Output Pins
+int red = 3;
+int green = 5;
+int blue = 6;
+
+
+void setup()  {
+} 
+
+void loop()  {
+ analogWrite(red, 180);
+ analogWrite(green,180);
+ analogWrite(blue,180);
+}
+
+
